da5e37d53c95dec4d5ea786b2745041bb064b310
Akash Tonne <atonne@scu.edu>
2019-12-03 02:11:28 -0800

    build structure

---
+++ build/precache-manifest.53dd6813ed0ed66f69d72d44cab893fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a86975dff27f21ebd0dacaee9cada589",
+    "url": "/static/media/arrow-back.a86975df.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f75f78e5ca91829bf438",
+    "url": "/static/js/main.f75f78e5.chunk.js"
+  },
+  {
+    "revision": "f52fed4c2febb163858c",
+    "url": "/static/js/1.f52fed4c.chunk.js"
+  },
+  {
+    "revision": "f75f78e5ca91829bf438",
+    "url": "/static/css/main.5f99a10c.chunk.css"
+  },
+  {
+    "revision": "f52fed4c2febb163858c",
+    "url": "/static/css/1.6c5c0302.chunk.css"
+  },
+  {
+    "revision": "17ee8663749e8d33a70d8b6467d35d53",
+    "url": "/index.html"
+  }
+];
